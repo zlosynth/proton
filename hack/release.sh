1e28767ffd966814736d7ebece8b738d9f869bf6
Petr Horacek <hrck@protonmail.com>
2022-04-30 18:20:57 +0200

    Introduce script for release updates
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -4,6 +4,8 @@ set -euo pipefail
 version=${1}
 
 sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
-sed -i "s/Rev .*/Rev \"v${version}\"/" hardware/Module.sch
-sed -i "s/gr_text \"board .*\"/gr_text \"board v${version}\"/" hardware/Module.kicad_pcb
-sed -i "s/rev .*/rev v${version})/" hardware/Module.kicad_pcb
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" lib/Cargo.toml
+sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" puredata/Cargo.toml
+sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_sch
+sed -i "s/gr_text \"board .*\" /gr_text \"board v${version}\" /" hardware/Module.kicad_pcb
+sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_pcb
