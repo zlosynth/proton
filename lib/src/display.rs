5796a536a6c60b550545e9c6522137a1cfd8d2c3
Petr Horacek <hrck@protonmail.com>
2022-05-01 13:31:36 +0200

    Draw scroll bar for module pages
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- lib/src/display.rs
+++ lib/src/display.rs
@@ -1,9 +1,13 @@
+#[allow(unused_imports)]
+use micromath::F32Ext;
+
 use alloc::vec;
 
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X12, MonoTextStyle},
     pixelcolor::BinaryColor,
     prelude::*,
+    primitives::{Line, PrimitiveStyle},
     text::Text,
 };
 use embedded_graphics_core::draw_target::DrawTarget;
@@ -13,6 +17,7 @@ use crate::store::{Module, Store};
 const PADDING_LEFT: i32 = 5;
 const FONT_HEIGHT: i32 = 12;
 const MODULES_PER_PAGE: usize = 5;
+const DISPLAY_HEIGHT: i32 = 64;
 
 pub struct Display<D> {
     pub display: D,
@@ -37,14 +42,22 @@ where
                 Module { name: "FOL" },
                 Module { name: "DIS" },
             ],
-            modules_page: 1,
+            modules_page: 0,
         };
 
         let (list_start, list_stop) = range_for_modules_page(&store.modules, store.modules_page);
         for (i, module) in store.modules[list_start..=list_stop].iter().enumerate() {
             draw_module(module, i, &mut self.display);
         }
+
+        draw_modules_scroll_bar(&store.modules, store.modules_page, &mut self.display);
     }
+}
+
+fn range_for_modules_page(modules: &[Module], modules_page: usize) -> (usize, usize) {
+    let list_start = modules_page * MODULES_PER_PAGE;
+    let list_stop = usize::min((modules_page + 1) * MODULES_PER_PAGE, modules.len()) - 1;
+    (list_start, list_stop)
 }
 
 fn draw_module<D: DrawTarget<Color = BinaryColor>>(module: &Module, index: usize, display: &mut D) {
@@ -58,8 +71,26 @@ fn draw_module<D: DrawTarget<Color = BinaryColor>>(module: &Module, index: usize
     .unwrap();
 }
 
-fn range_for_modules_page(modules: &[Module], modules_page: usize) -> (usize, usize) {
-    let list_start = modules_page * MODULES_PER_PAGE;
-    let list_stop = usize::min((modules_page + 1) * MODULES_PER_PAGE, modules.len()) - 1;
-    (list_start, list_stop)
+fn draw_modules_scroll_bar<D: DrawTarget<Color = BinaryColor>>(
+    modules: &[Module],
+    modules_page: usize,
+    display: &mut D,
+) {
+    let sections = (modules.len() as f32 / MODULES_PER_PAGE as f32).ceil() as i32;
+    let section_height = DISPLAY_HEIGHT / sections;
+
+    let line_start = modules_page as i32 * section_height;
+
+    let is_last_section = modules_page == sections as usize - 1;
+    let line_stop = if is_last_section {
+        DISPLAY_HEIGHT
+    } else {
+        (modules_page + 1) as i32 * section_height - 1
+    };
+
+    Line::new(Point::new(0, line_start), Point::new(0, line_stop))
+        .into_styled(PrimitiveStyle::with_stroke(BinaryColor::On, 1))
+        .draw(display)
+        .ok()
+        .unwrap();
 }
