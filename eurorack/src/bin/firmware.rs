9d456a685b58e099bc44147c376288bd0d28f1af
Petr Horacek <hrck@protonmail.com>
2022-05-12 18:32:32 +0200

    Extend RTIC stub to run blinky
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- eurorack/src/bin/firmware.rs
+++ eurorack/src/bin/firmware.rs
@@ -1,28 +1,52 @@
 #![no_main]
 #![no_std]
 
-use proton_eurorack as _; // global logger + panicking-behavior + memory layout
+use proton_eurorack as _; // global logger + panicking-behavior
 
-#[rtic::app(device = stm32h7xx_hal::pac, dispatchers = [EXTI0])]
+#[rtic::app(device = stm32h7xx_hal::pac, peripherals = true, dispatchers = [EXTI0])]
 mod app {
+    use systick_monotonic::*;
+
+    use daisy::led::{Led, LedUser};
+
+    #[monotonic(binds = SysTick, default = true)]
+    type Mono = Systick<1000>; // 1 kHz / 1 ms granularity
+
     #[shared]
     struct Shared {}
 
     #[local]
-    struct Local {}
-
-    #[init]
-    fn init(_: init::Context) -> (Shared, Local, init::Monotonics) {
-        defmt::info!("INIT");
-        foo::spawn().unwrap();
-
-        (Shared {}, Local {}, init::Monotonics())
+    struct Local {
+        led: LedUser,
     }
 
-    #[task]
-    fn foo(_: foo::Context) {
-        defmt::info!("FOO");
+    #[init]
+    fn init(cx: init::Context) -> (Shared, Local, init::Monotonics) {
+        defmt::info!("INIT");
 
-        proton_eurorack::exit();
+        let mono = Systick::new(cx.core.SYST, 480_000_000);
+
+        let dp = cx.device;
+        let board = daisy::Board::take().unwrap();
+        let ccdr = daisy::board_freeze_clocks!(board, dp);
+        let pins = daisy::board_split_gpios!(board, ccdr, dp);
+        let led = daisy::board_split_leds!(pins).USER;
+
+        foo::spawn(true).unwrap();
+
+        (Shared {}, Local { led }, init::Monotonics(mono))
+    }
+
+    #[task(local = [led])]
+    fn foo(cx: foo::Context, on: bool) {
+        defmt::info!("FOO: {:?}", on);
+
+        if on {
+            cx.local.led.on();
+            foo::spawn_after(1.secs(), false).unwrap();
+        } else {
+            cx.local.led.off();
+            foo::spawn_after(1.secs(), true).unwrap();
+        }
     }
 }
