29825d8bb063041c993afeef52bf77fa92a5834e
Petr Horacek <hrck@protonmail.com>
2022-05-28 15:15:06 +0200

    Fix release script to find all Cargo.toml
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- hack/release.sh
+++ hack/release.sh
@@ -4,8 +4,7 @@ set -euo pipefail
 version=${1}
 
 sed -i "s/## Unreleased/## Unreleased\n\n## ${version}/" CHANGELOG.md
-sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" lib/Cargo.toml
-sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/" puredata/Cargo.toml
+find -name Cargo.toml | xargs sed -i "s/version =.* # hack\/release.sh$/version = \"${version}\" # hack\/release.sh/"
 sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_sch
 sed -i "s/gr_text \"board .*\" /gr_text \"board v${version}\" /" hardware/Module.kicad_pcb
 sed -i "s/rev .*/rev \"v${version}\")/" hardware/Module.kicad_pcb
