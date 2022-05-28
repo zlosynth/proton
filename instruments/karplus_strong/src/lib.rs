9271d2e73fd170935d4792f38e34cfc33a6c05ee
Petr Horacek <hrck@protonmail.com>
2022-05-28 23:00:16 +0200

    Implement frequency+feedback control in karplus
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- instruments/karplus_strong/src/lib.rs
+++ instruments/karplus_strong/src/lib.rs
@@ -10,7 +10,9 @@ use proton_ui::reaction::Reaction;
 use proton_ui::state::*;
 
 const NAME: &str = "Karplus Strong";
-const CUTOFF_FREQUENCY_ATTRIBUTE: &str = "cutoff";
+const FREQUENCY_ATTRIBUTE: &str = "frequency";
+const CUTOFF_ATTRIBUTE: &str = "cutoff";
+const FEEDBACK_ATTRIBUTE: &str = "feedback";
 
 const MAX_SAMPLE_RATE: u32 = 48_000;
 const MIN_FREQUENCY: f32 = 40.0;
@@ -21,6 +23,8 @@ pub struct Instrument {
     noise: WhiteNoise,
     envelope: Ad,
     ring_buffer: RingBuffer<SAMPLES>,
+    frequency: f32,
+    feedback: f32,
     sample_rate: u32,
     countdown: u32,
 }
@@ -29,7 +33,9 @@ impl Instrument {
     pub fn initial_state() -> State {
         State::new(NAME)
             .with_attributes(&[
-                Attribute::new(CUTOFF_FREQUENCY_ATTRIBUTE).with_value_f32(ValueF32::new(0.3))
+                Attribute::new(FREQUENCY_ATTRIBUTE).with_value_f32(ValueF32::new(0.5)), // TODO: use default value
+                Attribute::new(CUTOFF_ATTRIBUTE).with_value_f32(ValueF32::new(0.3)),
+                Attribute::new(FEEDBACK_ATTRIBUTE).with_value_f32(ValueF32::new(0.9)),
             ])
             .unwrap()
     }
@@ -54,6 +60,8 @@ impl Instrument {
             noise,
             envelope,
             ring_buffer,
+            frequency: 100.0,
+            feedback: 0.9,
             sample_rate,
             countdown: u32::MAX,
         }
@@ -62,16 +70,18 @@ impl Instrument {
     pub fn populate(&mut self, buffer: &mut [f32]) {
         for x in buffer.iter_mut() {
             if self.countdown > self.sample_rate / 2 {
-                self.envelope
-                    .trigger(AdConfig::new().with_decay_time(0.005));
+                self.envelope.trigger(
+                    AdConfig::new().with_decay_time(self.frequency / self.sample_rate as f32),
+                );
                 self.countdown = 0;
             }
             self.countdown += 1;
 
             let new_sample = self.noise.pop() * self.envelope.pop();
-            // TODO: Interpolate delayed samples
-            let delayed_sample = self.ring_buffer.peek(self.sample_rate as i32 / -500);
-            let mixed_sample = self.svf.tick(new_sample + delayed_sample * 0.99);
+            let delayed_sample = self
+                .ring_buffer
+                .peek(-(self.sample_rate as i32) / self.frequency as i32);
+            let mixed_sample = self.svf.tick(new_sample + delayed_sample * self.feedback);
             self.ring_buffer.write(mixed_sample);
 
             *x = mixed_sample;
@@ -80,8 +90,14 @@ impl Instrument {
 
     pub fn execute(&mut self, command: Command) {
         match command {
-            Command::SetCutoffFrequency(value) => {
+            Command::SetCutoff(value) => {
                 self.svf.set_frequency(value);
+            }
+            Command::SetFrequency(value) => {
+                self.frequency = value;
+            }
+            Command::SetFeedback(value) => {
+                self.feedback = value;
             }
         }
     }
@@ -89,7 +105,9 @@ impl Instrument {
 
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Command {
-    SetCutoffFrequency(f32),
+    SetFrequency(f32),
+    SetCutoff(f32),
+    SetFeedback(f32),
 }
 
 impl TryFrom<Reaction> for Command {
@@ -98,8 +116,12 @@ impl TryFrom<Reaction> for Command {
     fn try_from(other: Reaction) -> Result<Self, Self::Error> {
         match other {
             Reaction::SetValue(attribute, value) => {
-                if attribute == CUTOFF_FREQUENCY_ATTRIBUTE {
-                    Ok(Command::SetCutoffFrequency(value * 5000.0))
+                if attribute == CUTOFF_ATTRIBUTE {
+                    Ok(Command::SetCutoff(value * 5000.0))
+                } else if attribute == FREQUENCY_ATTRIBUTE {
+                    Ok(Command::SetFrequency(value * 1000.0))
+                } else if attribute == FEEDBACK_ATTRIBUTE {
+                    Ok(Command::SetFeedback(0.9 + value / 10.0))
                 } else {
                     Err("cannot convert this reaction to a command")
                 }
@@ -119,12 +141,20 @@ mod tests {
         matches Err(_)
     )]
     #[test_case(
-        Reaction::SetValue(CUTOFF_FREQUENCY_ATTRIBUTE, 0.0) =>
-        Ok(Command::SetCutoffFrequency(0.0))
+        Reaction::SetValue(CUTOFF_ATTRIBUTE, 0.0) =>
+        Ok(Command::SetCutoff(0.0))
     )]
     #[test_case(
-        Reaction::SetValue(CUTOFF_FREQUENCY_ATTRIBUTE, 0.1) =>
-        Ok(Command::SetCutoffFrequency(500.0))
+        Reaction::SetValue(CUTOFF_ATTRIBUTE, 0.1) =>
+        Ok(Command::SetCutoff(500.0))
+    )]
+    #[test_case(
+        Reaction::SetValue(FREQUENCY_ATTRIBUTE, 0.1) =>
+        Ok(Command::SetFrequency(100.0))
+    )]
+    #[test_case(
+        Reaction::SetValue(FEEDBACK_ATTRIBUTE, 0.1) =>
+        Ok(Command::SetFeedback(0.90999997))
     )]
     fn it_converts_reaction_to_command(reaction: Reaction) -> Result<Command, &'static str> {
         reaction.try_into()
