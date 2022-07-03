7138bd5a3f2b2e82574657694b74e31b37a0e641
Petr Horacek <hrck@protonmail.com>
2022-07-03 22:39:53 +0200

    Add kudos to oversampling module
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- primitives/src/oversampling/mod.rs
+++ primitives/src/oversampling/mod.rs
@@ -1,3 +1,9 @@
+// Kudos to Nigel Redmon, www.earlevel.com
+// * https://www.earlevel.com/main/2010/11/23/towards-a-practical-oversampling-filter/
+// * https://www.earlevel.com/main/2010/12/05/building-a-windowed-sinc-filter/
+// * https://www.earlevel.com/main/2010/12/05/sample-rate-conversion-up/
+// * https://www.earlevel.com/main/2010/12/20/sample-rate-conversion-down/
+
 pub mod upsampling;
 
 mod coefficients;
