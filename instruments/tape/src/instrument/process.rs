b10fa8e13affe0a351e4d326e6b09e8383eef9b2
Petr Horacek <hrck@protonmail.com>
2022-07-04 18:13:30 +0200

    Make tape instrument chain more compact
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- instruments/tape/src/instrument/process.rs
+++ instruments/tape/src/instrument/process.rs
@@ -10,15 +10,14 @@ impl Instrument {
     pub fn process(&mut self, block: &mut [f32; 32], _randomizer: &mut impl Rand) {
         let block_copy = *block;
 
-        let input_signal = signal::from_iter(block_copy.into_iter());
-        let pre_gained_signal = input_signal.mul_amp(self.pre_gain.by_ref());
-        let mut hysteresis_signal = pre_gained_signal
+        let mut instrument = signal::from_iter(block_copy.into_iter())
+            .mul_amp(self.pre_gain.by_ref())
             .upsample(&mut self.upsampler)
             .map(|x| self.hysteresis.process(x))
             .downsample(&mut self.downsampler);
 
         block.iter_mut().for_each(|f| {
-            *f = hysteresis_signal.next();
+            *f = instrument.next();
         });
     }
 }
