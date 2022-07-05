4902e03682e74ffb452a1cafe1d3cab61ab17edb
Petr Horacek <hrck@protonmail.com>
2022-07-05 15:19:25 +0200

    Allow tape arguments to go outside range
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- instruments/tape/src/instrument/state.rs
+++ instruments/tape/src/instrument/state.rs
@@ -14,27 +14,27 @@ impl Instrument {
                 Attribute::new(attributes::PRE_GAIN).with_value_f32(
                     ValueF32::new(self.pre_gain.value())
                         .with_min(0.0)
-                        .with_max(1.0)
+                        .with_max(5.0)
                         .with_step(0.05)
                         .with_writter(percentage_writter),
                 ),
                 Attribute::new(attributes::DRIVE).with_value_f32(
                     ValueF32::new(self.hysteresis.drive())
-                        .with_min(0.0)
+                        .with_min(-5.0)
                         .with_max(5.0)
                         .with_step(0.1)
                         .with_writter(percentage_writter),
                 ),
                 Attribute::new(attributes::SATURATION).with_value_f32(
                     ValueF32::new(self.hysteresis.saturation())
-                        .with_min(0.0)
-                        .with_max(1.0)
+                        .with_min(-10.0)
+                        .with_max(2.0)
                         .with_step(0.05)
                         .with_writter(percentage_writter),
                 ),
                 Attribute::new(attributes::WIDTH).with_value_f32(
                     ValueF32::new(self.hysteresis.width())
-                        .with_min(0.0)
+                        .with_min(-10.0)
                         .with_max(1.0)
                         .with_step(0.02)
                         .with_writter(percentage_writter),
