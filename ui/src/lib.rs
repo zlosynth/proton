fbd884c87e237130800de514e6296c87021f955b
Petr Horacek <hrck@protonmail.com>
2022-07-10 12:54:55 +0200

    Improve formating of UI docs
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -12,12 +12,9 @@ pub mod reducer;
 pub mod state;
 pub mod view;
 
-// TODO NOTE
-// user input passes actions over a queue to state reducer
-// state reducer keeps State with Vecs etc
-// state reducer passes display state (Copy) via a #[task] argument to display
-// state reducer passes actions via queue to lib front, that handles interporation
-// CV input passes actions via queue to lib front
-// audio loop is owned by the lib itself
-//
-// TODO introduce Config that would be passed by plugins and used to create State
+// - user input passes actions over a queue to state reducer
+// - state reducer keeps State with Vecs etc
+// - state reducer passes display state (Copy) via a #[task] argument to display
+// - state reducer passes actions via queue to lib front, that handles interporation
+// - CV input passes actions via queue to lib front
+// - audio loop is owned by the lib itself
