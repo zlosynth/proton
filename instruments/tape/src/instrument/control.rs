b78ccd3df86cc948ddce8c22297659744b6418f8
Petr Horacek <hrck@protonmail.com>
2022-07-10 20:04:00 +0200

    Control tape through CV
    Signed-off-by: Petr Horacek <hrck@protonmail.com>

--- instruments/tape/src/instrument/control.rs
+++ instruments/tape/src/instrument/control.rs
@@ -3,5 +3,9 @@ use proton_control::input_snapshot::InputSnapshot;
 use super::Instrument;
 
 impl Instrument {
-    pub fn update_control(&mut self, _snapshot: InputSnapshot) {}
+    pub fn update_control(&mut self, snapshot: InputSnapshot) {
+        self.drive.set(snapshot.cv[0].value);
+        self.saturation.set(snapshot.cv[1].value);
+        self.width.set(snapshot.cv[2].value);
+    }
 }
